2a87bbe43f90af2f5af74879e33a4db308b20097
FawkesOficial <mario.lourenco.morte@gmail.com>
2025-12-17 18:10:12 +0000

    templates: add template for rust

---
+++ templates/rust.rs
@@ -0,0 +1,46 @@
+fn part1(input: &str) -> Option<u64> {
+    None
+}
+
+fn part2(_input: &str) -> Option<u64> {
+    None
+}
+
+fn main() {
+    let input = include_str!("../../../input.txt");
+
+    if let Some(p1_result) = part1(&input) {
+        println!("p1: {}", p1_result);
+    }
+    if let Some(p2_result) = part2(&input) {
+        println!("p2: {}", p2_result);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = r#"
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+"#;
+
+    #[test]
+    fn test_part1_example() {
+        assert_eq!(part1(EXAMPLE.trim()), Some(6));
+    }
+
+    #[test]
+    fn test_part2_example() {
+        assert_eq!(part2(EXAMPLE.trim()), None);
+    }
+}
